4edbb565bebc97cab78e5b6510f0de4427b7cf22
giorgos.georgopoulos <giorgos.georgopoulos@incelligent.net>
2026-05-17 23:34:17 +0300

    add logo asop diaas everywhere in my app

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -84,11 +84,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "static/"
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 LOGIN_REDIRECT_URL = "/tournaments/"
 LOGOUT_REDIRECT_URL = "/tournaments/"
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
