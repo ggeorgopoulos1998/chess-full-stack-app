2a8ce6c232785f85c926556caf73658f8c59fb40
giorgos.georgopoulos <giorgos.georgopoulos@incelligent.net>
2026-06-25 02:49:03 +0300

    Disable static compression

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -115,11 +115,11 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 
 STATIC_URL = "/static/"
